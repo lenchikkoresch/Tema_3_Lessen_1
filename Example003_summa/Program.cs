9fbae46afd28fcedc8bf4aa889170ebc50f8ab3e
lenschik.kor <lenschik.kor@yandex.ru>
2023-01-18 20:17:09 +0300

    Случайные числа исправлено

--- Example003_summa/Program.cs
+++ Example003_summa/Program.cs
@@ -1,7 +1,7 @@
 ﻿int numberA = new Random().Next(1, 10);
 Console.WriteLine(numberA);
 int numberB = new Random().Next(1, 10);
-Console.WreteLine(numberB);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
-Console.WriteLine (result);
+Console.WriteLine(result);
 
